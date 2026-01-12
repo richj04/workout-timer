686795565afa56d793bcd06b99ffa96b1122456e
Richard <richardl.ji04@gmail.com>
2026-01-11 17:54:10 -0700

    finished gacha calculator function untested

--- src/utils/gachaCalculator.js
+++ src/utils/gachaCalculator.js
@@ -11,28 +11,61 @@ export function gachaCalculator( chestType ){
         const crateOdds = advancedCrateOdds;
     }
 
-    
-
-    const newChimera = {};
+    const newChimera = [];
     const newChimeraSeed = "";
     const newChimeraTitle = "";
-    const newChimerName = "";
+    const newChimeraName = "";
 
+    //creates a part with random species and effect
     function createPart(){
         const newPart = {
             species: {},
             effect: {}
         }
         const partSpeciesKey = getRandomKey(speciesData);
-        newPart[species] = speciesData[partSpeciesKey];
+        newPart["species"] = speciesData[partSpeciesKey];
         const rarity = rarityCalculator(crateOdds);
-        
 
+        const effectPool = {
+            common: commonEffects,
+            rare: rareEffects,
+            epic: epicEffects,
+            legendary: legendaryEffects
+        }
 
+        const partEffectKey = getRandomKey(effectPool[rarity]);
+        newPart["effect"] = effectPool[rarity][partEffectKey];
+
+        return newPart;
     }
 
-    const newUpper = {};
-    const upperSpeciesKey = getRandomKey(speciesData);
-    newUpper = speciesData[upperSpeciesKey];
+    //maps to map index of for loop to object key name
+    const speciesNameMap = {
+        "0": "prefix",
+        "1": "infix",
+        "2": "suffix"
+    }
+    const effectNameMap = {
+        "0": "prefix",
+        "1": "adjective",
+        "2": "title"
+    }
+
+    //loop 3 times, creates 3 random parts
+    for(let i = 0; i < 3; i++){
+        newChimera[i] = createPart();
+        newChimeraSeed += (newChimera[i]["species"]["key"] + newChimera[i]["effect"]["key"]);
+
+        let titleKey = effectNameMap[i];
+        newChimeraTitle += newChimera[i]["effect"]["data"][`${titleKey}`];
+        let nameKey = speciesNameMap[i];
+        newChimeraName += newChimera[i]["species"]["data"][`${nameKey}`];
+
+        if(i != 2){
+            newChimeraTitle +=  " ";
+        }
+    }
+
+    
     
 }
